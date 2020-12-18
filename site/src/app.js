bc7602b3d55fdda5033ccffcac4b029dec8e9a9f
Facundojs <facuserra2002@gmailcom>
2020-12-17 23:46:38 -0300

    Comienzo del home usando mobile-first

--- site/src/app.js
+++ site/src/app.js
@@ -4,21 +4,18 @@ const app = express() ;
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
-
-app.get('/' , (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
-});
-// http://localhost:3000/header
-
-// Hice este archivo HTML&CSS solo para que queden el header&footer en formato fácil de copiar
-app.get('/header' , (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/header.html'))
-});
-
 app.listen(3000, () => {
     console.log('|==================================================================|');
     console.log('|--------------Servidor corriendo en el puerto 3000----------------|');
     console.log('|==================================================================|');
 });
 
-console.log(__dirname);
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+// http://localhost:3000/header acá pueden agarrar head, header, main y footer, pegarlo en su archivo HTML
+// Y ya les queda la pág. con las configuraciones y alineaciones, así solo trabajan el interior del main
+app.get('/header' , (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/header.html'))
+});
