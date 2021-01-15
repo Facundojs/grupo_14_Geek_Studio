c719ae696f79f5286ef6e2c9526c9ca6f1566313
Facundojs <facuserra2002@gmailcom>
2021-01-15 17:25:46 -0300

    Rutas y extensiones .ejs

--- site/src/app.js
+++ site/src/app.js
@@ -1,14 +1,18 @@
 const express = require('express') ;
 const path = require('path') ;
-const app = express() ;
+const app = express();
+
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
+
+app.set('view engine', 'ejs')
 
 app.listen(3000, () => {
     console.log('|==================================================================|');
     console.log('|--------------Servidor corriendo en el puerto 3000----------------|');
     console.log('|==================================================================|');
 });
+
 
 app.get('/header' , (req,res) => {
     res.sendFile(path.join(__dirname, '/views/header.html'))
