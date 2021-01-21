3716f836d26dbdace384a7a21ed970d59e43dfce
drakarisbelen <kari.belen@gmail.com>
2021-01-21 01:28:49 -0300

    saco el directorio el site - packajson lo deje en la raiz - setie la carpeta views  a /src/views para que la tome con heroku

--- src/database/products.js
+++ src/database/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const publicPath = path.resolve(__dirname, '../public');
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 module.exports = [
