a2e48e2dc06e116f989fbc640e5510853f7dee16
Facundojs <facuserra2002@gmailcom>
2021-01-27 19:43:10 -0300

    Str

--- site/src/app.js
+++ site/src/app.js
@@ -2,11 +2,11 @@ const express = require('express') ;
 const app = express();
 const path = require('path') ;
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
 app.set('view engine', 'ejs');
-// app.set('views', './src/views');
+// app.set('views', '../src/views');
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('|==================================================================|');
