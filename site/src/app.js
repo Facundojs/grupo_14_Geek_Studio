c81f713fbfd63116613c7dc132aabf23ade1d08b
MauroPdz <mauropedrozo1@gmail.com>
2021-01-30 16:41:57 -0300

    app.js

--- site/src/app.js
+++ site/src/app.js
@@ -6,11 +6,7 @@ const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
 app.set('view engine', 'ejs');
-<<<<<<< HEAD
 // app.set('views', '../src/views');
-=======
-//app.set('views', '../src/views');
->>>>>>> 1fd5fb3dfc69d0e7ace044c1e538e2004a9bac56
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('|==================================================================|');
