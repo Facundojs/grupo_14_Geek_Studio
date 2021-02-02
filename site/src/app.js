98f7ed5d414e0a8877d847488f6be4c2567cd867
drakarisbelen <kari.belen@gmail.com>
2021-02-01 23:54:20 -0300

    modifico set views en app.js para subir a heroku

--- site/src/app.js
+++ site/src/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
-// app.set('views', '../src/views');
+app.set('views', './views');
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('|==================================================================|');
