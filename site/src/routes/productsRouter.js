a8e26e8adf1573f8e3d8e3a2146c83f8dd23bacd
Facundojs <facuserra2002@gmailcom>
2021-02-04 22:12:54 -0300

    Más crud

--- site/src/routes/productsRouter.js
+++ site/src/routes/productsRouter.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
 const productController = require('../controllers/productsController')
 
-//Crud
+//Multer
+//Solo es la config, falta agregar a la ruta y configurar los controladores
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.join(__dirname, '../../public/img/products/'));
+    },
+    filename: (req, file, callback) => {
+        //console.log(file);
+        // https://www.npmjs.com/package/uuidv4
+        callback(null, 'product-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage });
 
+//Crud
 router.get('/', productController.products);
 router.get('/detalle', productController.detalle);//********// 
 //router.get('/:id',);/**/
