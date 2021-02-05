5dcbdc5dae3d80ffb10a2859fc1ec3f4cad34572
Facundojs <facuserra2002@gmailcom>
2021-02-04 23:29:18 -0300

    Products.json

--- site/src/routes/productsRouter.js
+++ site/src/routes/productsRouter.js
@@ -19,7 +19,7 @@ const upload = multer({ storage });
 
 //Crud
 router.get('/', productController.products);
-router.get('/:id', productController.detalle);
+router.get('/detalle/:id', productController.detalle);
 router.get('/crear', productController.create);
 router.post('/crear', productController.store);
 router.get('/:id/editar', productController.edit);
