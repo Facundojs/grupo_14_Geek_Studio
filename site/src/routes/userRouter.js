60dbc6c935d61d34c1e066fef97985b867c0d959
drakarisbelen <kari.belen@gmail.com>
2021-02-06 16:38:41 -0300

    Listado users: http://localhost:3000/users/index, Create Users (registrarse), Edit Users por ahora al confirmar me graba pero no me esta redireccionando. KE

--- site/src/routes/userRouter.js
+++ site/src/routes/userRouter.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+router.get('/index', userController.index);
+router.get('/login', userController.login);
+router.get('/crear',userController.crear); //router.get('/register',userController.register);
+router.post('/', userController.store); //post para grabar
+router.get('/:id', userController.show); //get para mostrar
 
-router.get('/login', userController.login)
-router.get('/register',userController.register)
+//Edit
+router.get('/:id/edit', userController.edit); //get para editar
+//router.post('/:id/edit', userController.edit); //get para editar
+router.post('/:id/edit', userController.update); //post para editar
+
+//router.delete('/borrando', productController.destroy);
+
 router.get('/recover-pass', userController.recoverPass);
 
 module.exports = router;
