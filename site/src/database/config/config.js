6ee0d5e5108bc8278c974ede210266d398a23e08
Facundojs <facuserra2002@gmailcom>
2021-03-03 11:17:03 -0300

    sequelize configs

---
+++ site/src/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "development": {
+  "username": "root",
+  "password": "root",
+  "database": "store",
+  "host": "localhost",
+  "dialect": "mysql",
+  "port": 8889
+  },
+  "test": {
+  "username": "root",
+  "password": null,
+  "database": "database_test",
+  "host": "127.0.0.1",
+  "dialect": "mysql"
+  },
+  "production": {
+  "username": "root",
+  "password": null,
+  "database": "database_production",
+  "host": "127.0.0.1",
+  "dialect": "mysql"
+  }
+ }
