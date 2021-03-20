ebfd87ebf834d318c05adad0211d537525e27102
MauroPdz <mauropedrozo1@gmail.com>
2021-03-20 20:21:34 -0300

    CRUD y validations

--- database/mysql_script.sql
+++ database/mysql_script.sql
@@ -16,8 +16,13 @@ CREATE TABLE `products` (
 	`price` DECIMAL NOT NULL,
 	`image` VARCHAR(255) NOT NULL,
 	`description` TEXT NOT NULL,
-	PRIMARY KEY (`id`),
-    FOREIGN KEY (category_id) REFERENCES categories (id)
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_type` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`type_name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `users` (
@@ -31,19 +36,13 @@ CREATE TABLE `users` (
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `user_type` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`type_name` VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`)
-);
+ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`user_type_id`) REFERENCES `user_type`(`id`);
+
+ALTER TABLE `products` ADD CONSTRAINT `products_fk0` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`);
 
 
-## ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`user_type_id`) REFERENCES `user_type`(`id`);
-
-## ALTER TABLE `products` ADD CONSTRAINT `products_fk0` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`);
-
-
-
-
-
-
+-- INSERT INTO categories values (1, "Componentes");
+-- INSERT INTO categories values (2, "Perifericos");
+-- INSERT INTO categories values (3, "Gabinetes");
+-- INSERT INTO categories values (4, "Sillas");
+-- INSERT INTO categories values (5, "Combos");
