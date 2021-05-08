e409c7c2a4a9e3ab53acf6cdd2658937c0396624
MauroPdz <mauropedrozo1@gmail.com>
2021-05-08 16:25:41 -0300

    Boton de scroll

---
+++ site/public/js/gotop.js
@@ -0,0 +1,18 @@
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop)
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector(".go-top-container")
+        .classList.add('show');
+    } else {
+        document.querySelector(".go-top-container")
+        .classList.remove('show');
+    }
+}
+
+document.querySelector(".go-top-container")
+.addEventListener('click', () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
