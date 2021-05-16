1076b2d19792bb1390ab5afabaea8a2071f3521f
drakarisbelen <kari.belen@gmail.com>
2021-05-16 13:43:34 -0300

    links del home

--- database/seeders/20210507060040-products_combos.js
+++ database/seeders/20210507060040-products_combos.js
@@ -9,8 +9,7 @@ module.exports = {
         discount: 20,
         category_id: 5,
         image: "pc-gamer-amd-athlon-3000g--8gb--1tb--kit-0.jpg",
-        features:
-          "Silla Gamer Kanji Dooku KJ-488TB 180 Grados Apoya Pies Black Blue ",
+        features: "PC GAMER AMD ATHLON 3000G - 8GB - 1TB ",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -21,8 +20,7 @@ module.exports = {
         discount: 20,
         category_id: 5,
         image: "pc-intel-core-i3-10100--h410--8gb--1tb--kit-0.jpg",
-        features:
-          "Silla Gamer Kanji Dooku KJ-488TB 180 Grados Apoya Pies Black Blue ",
+        features: "PC INTEL CORE I3 10100 + H410 + 8GB + 1TB + KIT ",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -34,7 +32,7 @@ module.exports = {
         category_id: 5,
         image: "pc-empresa-intel-i5-9400f--16gb--ssd--1tb--win10-pro-0.jpg",
         features:
-          "Silla Gamer Kanji Dooku KJ-488TB 180 Grados Apoya Pies Black Blue ",
+          "PC EMPRESA INTEL I5 9400F - 16GB - SSD + 1TB - WIN10 PRO - Ideal para juegos",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
